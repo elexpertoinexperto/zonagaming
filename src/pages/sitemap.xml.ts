9fe060193ad76eb4c0856c36fa132851d13d784e
ElExpertoInexperto <elexpertoinexperto@gmail.com>
2026-06-07 16:53:19 -0500

    home sin slash al final sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -34,7 +34,7 @@ export const GET: APIRoute = () => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>${site}/</loc>
+    <loc>${site}</loc>
     <priority>1.0</priority>
   </url>
   <url>
